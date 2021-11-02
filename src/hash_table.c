6383e92bfe10965779b11604788d43e3b2b902ac
ztjhz <59118459+ztjhz@users.noreply.github.com>
2021-11-02 20:07:34 +0800

    added insert and search

--- src/hash_table.c
+++ src/hash_table.c
@@ -3,6 +3,8 @@
 
 #include "hash_table.h"
 
+
+
 // create a new hash table item
 static ht_item* ht_new_item(const char* k, const char* v) {
     ht_item* new_item = malloc(sizeof(ht_item));
@@ -57,3 +59,43 @@ static int ht_hash(const char* s, const int a, const int m) {
     }
     return (int) hash;
 }
+
+// double hashing makes use of two hash functions to calculate the index of an item to be stored after a collision
+static int ht_get_hash(
+    const char* s, const int num_buckets, const int attempt
+) {
+    const int hash_a = ht_hash(s, HT_PRIME_1, num_buckets);
+    const int hash_b = ht_hash(s, HT_PRIME_2, num_buckets);
+    return (hash_a + (attempt * (hash_b + 1))) % num_buckets;
+}
+
+void ht_insert(ht_hash_table* ht, const char* key, const char* value) {
+    ht_item* item = ht_new_item(key, value);
+    int index = ht_get_hash(item->key, ht->size, 0);
+    ht_item* curr_item = ht->items[0];
+    int i = 1;
+    while (curr_item != NULL) {
+        index = ht_get_hash(item->key, ht->size, i);
+        curr_item = ht->items[index];
+        i++;
+    }
+    ht->items[index] = item;
+    ht->count++;
+}
+
+char* ht_search(ht_hash_table* ht, const char* key) {
+    int index = ht_get_hash(key, ht->size, 0);
+    ht_item* item = ht->items[index];
+    int i = 1;
+
+    while (item != NULL) {
+        if (strcmp(item->key, key) == 0) {
+            return item->value;
+        }
+        index = ht_get_hash(key, ht->size, i);
+        item = ht->items[index];
+        i++;
+    }
+    return NULL;
+}
+
--- src/hash_table.h
+++ src/hash_table.h
@@ -11,3 +11,9 @@ typedef struct {
     ht_item** items;
 } ht_hash_table;
 
+#define HT_PRIME_1 131
+#define HT_PRIME_2 137
+
+void ht_insert(ht_hash_table* ht, const char* key, const char* value);
+char* ht_search(ht_hash_table* ht, const char* key);
+void ht_delete(ht_hash_table* h, const char* key);
